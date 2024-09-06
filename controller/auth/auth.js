2c48c1d6cdddf834221a7158f0a9bd651cb16d78
DawrAli33285 <lemightyeagle@gmail.com>
2024-09-06 16:30:17 +0500

    updates

--- controller/auth/auth.js
+++ controller/auth/auth.js
@@ -1,6 +1,7 @@
 let userModel=require('../../models/user')
 let jwt=require('jsonwebtoken')
 const { v4: uuidv4 } = require('uuid')
+let nodemailer=require('nodemailer')
 module.exports.register=async(req,res)=>{
 let {email,password,userName}=req.body;
     try{
@@ -99,6 +100,130 @@ return res.status(200).json({
 
     }catch(error){
         console.log(error.message)
+        return res.status(400).json({
+            error:"Server error please try again"
+        })
+    }
+}
+
+
+
+module.exports.resetPassword=async(req,res)=>{
+    let {email}=req.body;
+console.log(email)
+try{
+
+    let userFound=await userModel.findOne({email})
+    if(!userFound){
+        return res.status(400).json({
+            error:"User not found"
+        })
+    }
+let token=await jwt.sign({email:userFound.email},process.env.JWT_KEY)
+const emailHtmlContent = `
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 20px;
+  }
+  .container {
+    max-width: 600px;
+    margin: auto;
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.05);
+  }
+  .header {
+    color: #333;
+    text-align: center;
+  }
+  .review {
+    background-color: #f9f9f9;
+    border-left: 4px solid #007BFF;
+    margin: 20px 0;
+    padding: 20px;
+    border-radius: 4px;
+  }
+  .rating {
+    text-align: right;
+    font-size: 18px;
+    font-weight: bold;
+    color: #ff9500;
+  }
+</style>
+</head>
+<body>
+
+<div class="container">
+  <div class="header">
+    <h2>Welcome to Our Platform</h2>
+  </div>
+  <div class="review">
+    <p>Hello,</p>
+    <p>Welcome to our platform! We are thrilled to have you on board.</p>
+   
+  </div>
+  <div>
+   <p>Please click on the link to go to reset password page</p>
+   <a href="https://bento-black.vercel.app/resetpassword?token=${token}">https://bento-black.vercel.app/resetpassword?token=${token}</a>
+  </div>
+</div>
+
+</body>
+</html>
+`;
+const transporter=nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port:465,
+    secure:true,
+    auth: {
+        user:process.env.email,
+        pass: process.env.email_pass,
+      },
+})
+const mailOptions = {
+    from:process.env.email,
+    to: email, 
+    subject: "Reset password link",
+    html: emailHtmlContent,
+};
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.error('Error sending email:', error);
+    }
+    if(info){
+        console.log(info)
+        return res.status(200).json({
+            message:"Email verification sent successfully"
+        })
+    }
+   
+});
+
+}catch(e){
+    return res.status(400).json({
+        error:"Server error please try again"
+    })
+}
+}
+
+
+module.exports.newPassword=async(req,res)=>{
+    let {password,token}=req.body;
+
+    try{
+   let user=await jwt.verify(token,process.env.JWT_KEY)
+  await userModel.updateOne({email:user.email},{password})
+  return res.status(200).json({
+    message:"Passworc updated sucessfully"
+  })
+    }catch(e){
         return res.status(400).json({
             error:"Server error please try again"
         })
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,7 +1,9 @@
 let router=require('express').Router();
-let {register,login,socialLogin,socialRegister}=require('../../controller/auth/auth')
+let {register,resetPassword,newPassword,login,socialLogin,socialRegister}=require('../../controller/auth/auth')
 router.post('/register',register)
 router.post('/login',login)
 router.post('/socialLogin',socialLogin)
 router.post('/socialRegister',socialRegister)
+router.post('/resetPassword',resetPassword)
+router.post('/newPassword',newPassword)
 module.exports=router;
